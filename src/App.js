158e426e30f6a3f3ee791d18e10f7006630bcb35
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-20 20:36:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from "react";
-import Child from "./Child";
-import Header from "./Header";
+import Nav from "./Nav"; 
+import Hero from "./Hero"; 
+
 function App() {
-  const data = [
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ajit Mengal", Age: 22, email: "ajitmengal2016@gmail.com" },
-    { Name: "Punam Bhat", Age: 23, email: "punambhat2016@gmail.com" },
-    { Name: "Sahil bhutambre", Age: 21, email: "sahilbhutambre2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-    { Name: "Ruturaj Mengal", Age: 21, email: "ruturajmengal2016@gmail.com" },
-  ];
-  return (
-    <div className="h-full bg-gradient-to-br from-red-500 to-blue-600 p-2 flex flex-col gap-4 justify-center">
-      <Header />
-      <div className="grid grid-cols-4 gap-[1rem] h-fit w-fit">
-        {data.map((e, i) => {
-          return <Child Name={e.Name} age={e.Age} email={e.email} />;
-        })}
-      </div>
-    </div>
-  );
+  return <div>
+    <Nav />
+    <Hero />
+  </div>;
 }
 
 export default App;
