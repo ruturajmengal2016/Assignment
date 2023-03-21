a63a3fa3650660bce4292f48c729afc337cb0b48
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-21 10:08:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import Hero from './Hero'
-import Nav from './Nav'
- 
-function App() {
-  return (
-    <div className='h-screen p-5 flex flex-col gap-3'>
-      <Nav />
-      <Hero />
-    </div>
-  )
-=======
 import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
@@ -21,7 +8,6 @@ function App() {
     <Header />
     <Hero />
   </div>;
->>>>>>> master
 }
 
 export default App
