4e0d452044cb670ffa12037a5c0df4e2fea91bbb
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-22 11:31:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import Header from "./Header";
-import Hero from "./Hero";
-
-
+import React from 'react'
 function App() {
-  return <div className="h-screen bg-red-500 sm:bg-blue-500">
-    <Header />
-    <Hero />
-  </div>;
+  return (
+    <div className='h-screen bg-red-400 flex justify-center w-screen'>
+      <div className='bg-white self-center overflow-hidden flex gap-3 p-3 justify-center h-[25rem]'>
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+        <div className='shadow-sm rounded-md w-96 self-center bg-blue-400 h-64 shadow-gray-600 flex justify-start flex-col'><img src='' alt="" className='h-11 w-11 self-center mt-14 mb-8'/><span className='self-center'>Name</span><span className='self-center'>Designation</span><span className='self-center'>Exp.</span></div> 
+      </div>
+    </div>
+  )
 }
 
 export default App
