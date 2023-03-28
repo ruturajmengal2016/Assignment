5b7f347ef507b6bcfa382c2f2d6290295a77d14f
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-28 20:02:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Link,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
+import React from 'react'
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-    )
-  );
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-const Root = () => {
-  return (
-    <>
-      <div className="w-screen bg-blue-500 flex justify-end gap-12 box-border pr-[6rem] h-[3rem] items-center">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/login">log In</Link>
-      </div>
-      <div>
-        <Outlet /> 
-      </div>
-    </>
-  );
-};
-export default App;
+export default App
