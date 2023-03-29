27088abfbdeb59bfa7091af87b2eda5b1726c7e2
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-29 16:32:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import { useRef, useState } from "react";
-import Styles from "./Styles/App.module.scss";
+import React from 'react'
+import Guess from './Guess'
 
 function App() {
-  const [number, setNumber] = useState(0);
-  const input = useRef()
-  const handleClick = (number) => {
-    if (number === "50") {
-      alert("Your guess is right!");
-    } else if (number < 50) {
-      alert("Your guess is too small"); 
-    } else if (number > 50) {
-      alert("Your guess is too big");
-    }
-  };
   return (
-    <div className={Styles.root}>
-      <div className={Styles.first_div}>
-        <input
-          type="text"
-          placeholder="Guess the Number.."
-          onChange={(event) => {
-            setNumber(event.target.value);
-          }} ref={input}
-        />
-        <button
-          onClick={() => {
-            handleClick(number);
-          }}
-        >
-          Match
-        </button>
-      </div>
+    <div>
+      <Guess />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Guess.js
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import Styles from "./Styles/Guess.module.scss";
+
+function Guess() {
+  const [number, setNumber] = useState(0);
+  const input = useRef()
+  const handleClick = (number) => {
+    if (number === "50") {
+      alert("Your guess is right!");
+    } else if (number < 50) {
+      alert("Your guess is too small"); 
+    } else if (number > 50) {
+      alert("Your guess is too big");
+    }
+  };
+  return (
+    <div className={Styles.root}>
+      <div className={Styles.first_div}>
+        <input
+          type="text"
+          placeholder="Guess the Number.."
+          onChange={(event) => {
+            setNumber(event.target.value);
+          }} ref={input}
+        />
+        <button
+          onClick={() => {
+            handleClick(number);
+          }}
+        >
+          Match
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Guess;
