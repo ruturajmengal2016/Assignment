286c6a2d675350be7c5fb657c505732a5b0f06ab
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-29 16:50:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react'
-import Guess from './Guess'
+import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Guess from "./Guess";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index path="/home" element={<Home />} />
+        <Route index path="/about" element={<About />} />
+        <Route index path="/login" element={<Login />} />
+        <Route index path="/register" element={<Register />} />
+        <Route path="/guess" element={<Guess />} />
+      </Route>
+    )
+  );
   return (
     <div>
-      <Guess />
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/guess">Guess</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+export default App;
