1f73068927986a34ec12db1ce120692e534a76f8
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-29 17:26:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Styles from "./Styles/App.module.scss";
 
 function App() {
   const router = createBrowserRouter(
@@ -26,16 +27,16 @@ function App() {
     )
   );
   return (
-    <div>
+    <>
       <RouterProvider router={router} />
-    </div>
+    </>
   );
 }
 
 const Root = () => {
   return (
     <>
-      <div>
+      <div className={Styles.navigation}>
         <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/login">Login</Link>
