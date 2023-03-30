c72c04e5721beb4839397d0f2a01da10bad3d0df
Ruturaj <ruturajmengal2016@gmail.com>
2023-03-30 16:12:52 +0530

    commit

--- src/Pages/First.js
+++ src/Pages/First.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import Style from '../Styles/First.module.scss'
+import React, { useEffect, useState } from "react";
+import Styles from "../Styles/First.module.scss";
 function First() {
+  const [count, setCount] = useState();
+  useEffect(() => {
+    fun();
+  }, []);
+  const fun = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const image = await response.json();
+    setCount(image.message);
+  };
+  console.log(count);
   return (
-    <div>First</div>
-  )
+    <div className={Styles.root}>
+      <div className={Styles.container}>
+        <div className={Styles.left}>
+          {count ? (
+            <>
+              <img src={count} alt="" />
+            </>
+          ) : (
+            <div>Loading....</div>
+          )}
+        </div>
+        <div className={Styles.right}>
+          <button onClick={fun}>Click</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default First
+export default First;
