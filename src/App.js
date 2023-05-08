bf27b121f53cec7c3afe1c7daf11c85b8dec0d68
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-08 22:41:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,57 +1,134 @@
-import * as React from "react";
+import React from "react";
 import Box from "@mui/material/Box";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
-import FormControl from "@mui/material/FormControl";
-import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
-import Typography from "@mui/material/Typography";
-export default function InputAdornments() {
-  const [value, setValue] = React.useState(0);
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
+import io from "socket.io-client";
+import { TextField } from "@mui/material";
+import ImageIcon from "@mui/icons-material/Image";
+import IconButton from "@mui/material/IconButton";
+import TextFormatIcon from "@mui/icons-material/TextFormat";
+const socket = io("http://localhost:3001");
+const App = () => {
+  const [answer, setAnswer] = React.useState();
+  const [change, setChange] = React.useState(true);
+  const [messages, setMessages] = React.useState([]);
+  const name = React.useMemo(() => {
+    return prompt("Enter your name");
+  }, []);
+  function handleSubmit() {
+    if (typeof answer === "object") {
+      const reader = new FileReader();
+      reader.readAsDataURL(answer);
+      reader.addEventListener(
+        "load",
+        (e) => {
+          const data = e.target.result;
+          socket.emit("send message", { data: data, name: name });
+        },
+        true
+      );
+    } else {
+      socket.emit("send message", { data: answer, name: name });
+    }
+  }
+  React.useEffect(() => {
+    socket.on("message", (data) => {
+      setMessages([...messages, data]);
+    });
+  }, [messages]);
   return (
     <Box
+      component="div"
       sx={{
-        position: "relative",
         display: "flex",
-        flexWrap: "wrap",
-        alignItems: "center",
         height: "100vh",
         justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
-      <Typography
-        variant="h2"
-        sx={{ position: "absolute", top: "25%", fontFamily: "revert-layer" }}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          bgcolor: "lightcoral",
+          width: 500,
+          height: 500,
+          maxHeight: "fit-content",
+        }}
       >
-        pixel to rem conversion
-      </Typography>
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-        <OutlinedInput
-          id="outlined-adornment-weight"
-          endAdornment={<InputAdornment position="end">px</InputAdornment>}
-          aria-describedby="outlined-weight-helper-text"
-          inputProps={{
-            "aria-label": "pixel",
-          }}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <FormHelperText id="outlined-weight-helper-text">Pixel</FormHelperText>
-      </FormControl>
-      <DoubleArrowIcon sx={{ marginBottom: "1rem" }} />
-      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-        <OutlinedInput
-          readOnly
-          id="outlined-adornment-weight"
-          endAdornment={<InputAdornment position="end">rem</InputAdornment>}
-          aria-describedby="outlined-weight-helper-text"
-          inputProps={{
-            "aria-label": "rem",
-          }}
-          value={value / 16}
-        />
-        <FormHelperText id="outlined-weight-helper-text">rem</FormHelperText>
-      </FormControl>
+        <Box
+          component="div"
+          height="90%"
+          width="100%"
+          sx={{ bgcolor: "#f44336", overflowY: "auto", padding: "0.5rem 0rem" }}
+        >
+          {messages.map((ele, ind) => {
+            return <ChatBox answer={ele.data} key={ind} name={ele.name} />;
+          })}
+        </Box>
+        <Box height="10%" width="100%" sx={{ display: "flex" }}>
+          <IconButton
+            onClick={() => {
+              setChange(!change);
+            }}
+          >
+            {change ? <TextFormatIcon /> : <ImageIcon />}
+          </IconButton>
+          {change ? (
+            <TextField
+              name="userName"
+              variant="outlined"
+              type="file"
+              sx={{ width: "85%" }}
+              onChange={(e) => {
+                setAnswer(e.target.files[0]);
+              }}
+            />
+          ) : (
+            <TextField
+              name="userName"
+              variant="outlined"
+              type="text"
+              sx={{ width: "85%" }}
+              onChange={(e) => {
+                setAnswer(e.target.value);
+              }}
+            />
+          )}
+
+          <Button
+            variant="contained"
+            sx={{ width: "15%" }}
+            onClick={handleSubmit}
+          >
+            <SendIcon />
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+function ChatBox({ answer, name }) {
+  return (
+    <Box
+      component="div"
+      width="50%"
+      sx={{
+        bgcolor: "lightblue",
+        minHeight: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        marginBottom: "1rem",
+        borderRadius: "0.5rem",
+        padding: "0.75rem",
+        textAlign: "start",
+      }}
+    >
+      <span>{name}</span>
+      {answer.length > 1000 ? <img src={answer} alt="photos" /> : answer}
     </Box>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Style from './App.module.scss'
 import App from "./App";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Outlet />}>
+      <Route path="/" element={<App />} />
+    </Route>
+  )
+);
 root.render(
   <React.Fragment>
-   <App />
+    <RouterProvider router={router} />
   </React.Fragment>
 );
