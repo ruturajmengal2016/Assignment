c67a00e47a97d3b279f2217b955140115490ff11
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-18 09:04:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,71 +1,113 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { useSelector, useDispatch } from "react-redux";
-import Question from "./components/Question";
-import Options from "./components/Options";
-import Button from "@mui/material/Button";
-import { setAnswers } from "./Redux/slice";
-export default function App() {
-  const [answer, setAnswer] = React.useState(0);
-  const [marks, setMarks] = React.useState(0);
-  const value = useSelector((state) => state.question.value);
-  const [next, setNext] = React.useState(0);
-  const dispatch = useDispatch();
-  const handleAnswer = () => {
-    dispatch(setAnswers({ answer: answer }));
-  };
-  const setHandleAnswer = (event) => {
-    setAnswer(event.target.value);
-  };
+import { Button, Typography } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+export default function BoxSx() {
+  const [temperature, setTemperature] = React.useState(0);
   return (
     <Box
       sx={{
         height: "100vh",
-        display: {
-          xs: "none",
-          sm: "flex",
-        },
+        display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column ",
       }}
     >
+      <Typography variant="h2">Temperature Controller</Typography>
       <Box
         sx={{
-          height: 500,
-          width: 500,
+          height: "50%",
+          width: "25%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "lightgray",
           boxSizing: "border-box",
-          padding: "1rem",
-          backgroundColor: "lightblue",
-          display: {
-            xs: "none",
-            sm: "flex",
-          },
+          padding: "1.5rem",
+          borderRadius: "2rem",
           flexDirection: "column",
         }}
       >
-        <Question question={value[next].question} />
-        <Options
-          options={value[next].options}
-          question={value[next].question}
-          setHandleAnswer={setHandleAnswer}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            if (value[next].answer == answer) setMarks(marks + 5);
-            if (next < value.length - 1) {
-              setNext(next + 1);
-            } else {
-              alert(`Your Total Marks are ${marks}`);
-              setNext(0);
-              setMarks(0);
-            }
-            handleAnswer();
+        <Box
+          sx={{
+            height: "60%",
+            width: "60%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor:
+              temperature > 20
+                ? temperature > 30
+                  ? temperature > 50
+                    ? "red"
+                    : "orange"
+                  : "yellow"
+                : "lightblue",
+            borderRadius: "50%",
           }}
         >
-          {next < value.length - 1 ? "Next" : "Submit"}
-        </Button>
+          <Typography variant="h4">
+            {temperature} <sup>o</sup>C
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            height: "20%",
+            width: "80%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            boxSizing: "border-box",
+            padding: "0rem 1rem",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: "1.5rem",
+              bgcolor: "red",
+              "&:hover": {
+                bgcolor: "red",
+              },
+            }}
+            onClick={() => {
+              if (temperature) setTemperature(temperature - 10);
+            }}
+          >
+            -
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ fontSize: "1.5rem" }}
+            onClick={() => {
+              if (temperature !== 100) setTemperature(temperature + 10);
+            }}
+          >
+            +
+          </Button>
+        </Box>
       </Box>
+      <Stack spacing={2} sx={{ width: "100%" }}>
+        <Snackbar
+          open={temperature > 19 && temperature < 49 && true}
+          autoHideDuration={1000}
+        >
+          <Alert severity="warning" sx={{ width: "100%" }}>
+            You are above the normal room temperatue!
+          </Alert>
+        </Snackbar>
+        <Snackbar open={temperature > 49 && true} autoHideDuration={1000}>
+          <Alert severity="error" sx={{ width: "100%" }}>
+            Danger!
+          </Alert>
+        </Snackbar>
+      </Stack>
     </Box>
   );
 }
--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { questionSlice } from "./slice";
-
-export default configureStore({
-  reducer: { question: questionSlice.reducer },
-});
