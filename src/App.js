f688bd7323ff28e26264029b8ef73787c90efd77
Ruturaj <ruturajmengal2016@gmail.com>
2023-05-18 16:18:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
+import { Box, IconButton, Typography } from "@mui/material";
 import React from "react";
-import quotes from "./Quotes.json";
-import { Box, Typography } from "@mui/material";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const App = () => {
+  const [counter, setCounter] = React.useState(0);
   return (
     <Box
-      component="div"
       sx={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
-        gap: "5rem",
+        height: "80vh",
       }}
     >
-      <Typography variant="h3">Motivational quotes</Typography>
-      <Typography
-        variant="h4"
+      <Box
+        component="div"
         sx={{
-          width: "50%",
-          minHeight: "fit-content",
-          bgcolor: "lightcoral",
-          boxSizing: "border-box",
-          padding: "0.5rem",
-          borderRadius: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          backgroundColor: "lightcoral",
+          width: "20%",
         }}
       >
-        {quotes.quotes[Math.floor(Math.random() * (9 - 0 + 1)) + 0]}
-      </Typography>
+        <IconButton
+          onClick={() => {
+            setCounter(counter + 10);
+          }}
+        >
+          <ArrowDropUpIcon
+            sx={{
+              color: "green",
+              fontSize: "5rem",
+            }}
+          />
+        </IconButton>
+        <Typography variant="h4">{counter}</Typography>
+        <IconButton
+          onClick={() => {
+            setCounter(counter - 10);
+          }}
+        >
+          <ArrowDropDownIcon
+            sx={{
+              color: "red",
+              fontSize: "5rem",
+            }}
+          />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
